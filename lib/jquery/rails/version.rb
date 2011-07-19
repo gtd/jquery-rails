2a00259da574bc3d9e478766b4d710edf81b412e
Gabe da Silveira <gabe@websaviour.com>
2011-07-19 12:20:32 -0700

    Avoid version double load :path or :git bundling

--- lib/jquery/rails/version.rb
+++ lib/jquery/rails/version.rb
@@ -1,8 +1,10 @@
 module Jquery
   module Rails
-    VERSION = "1.0.12"
-    JQUERY_VERSION = "1.6.2"
-    JQUERY_UI_VERSION  = "1.8.14"
-    JQUERY_UJS_VERSION = "0e742688c633bda274f8a09bf3248ffe33ecaf92"
+    unless defined?(::Jquery::Rails::VERSION)
+      VERSION = "1.0.12"
+      JQUERY_VERSION = "1.6.2"
+      JQUERY_UI_VERSION  = "1.8.14"
+      JQUERY_UJS_VERSION = "0e742688c633bda274f8a09bf3248ffe33ecaf92"
+    end
   end
 end
